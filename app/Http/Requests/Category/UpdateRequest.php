06a0258ce7d867a43bfe8e02d769bb73d5bfa1f9
Piero Bayona <pierodev21@gmail.com>
2023-10-18 21:17:07 -0500

    crear modelo categorias

---
+++ app/Http/Requests/Category/UpdateRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests\Category;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:50',
+            'description' => 'nullable|string|max:250',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'El campo nombre es obligatorio.',
+            'name.string' => 'El campo nombre debe ser una cadena de texto.',
+            'name.max' => 'El campo nombre no debe exceder los 50 caracteres.',
+            'description.string' => 'El campo descripción debe ser una cadena de texto.',
+            'description.max' => 'El campo descripción no debe exceder los 250 caracteres.',
+        ];
+    }
+}
