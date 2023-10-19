0d735e9f05203eca2aea1839dd7349cd7988e530
Piero Bayona <pierodev21@gmail.com>
2023-10-18 21:39:37 -0500

    Provedeedores parte backend inicio

---
+++ app/Http/Controllers/ProviderController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Provider\StoreRequest;
+use App\Http\Requests\Provider\UpdateRequest;
+use App\Models\Provider;
+
+class ProviderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $providers = Provider::get();
+        return view('admin.provider.index',compact('providers'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('admin.provider.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRequest $request)
+    {
+        Provider::create($request->all());
+        return redirect()->route('providers.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Provider $provider)
+    {
+        return view('admin.provider.show',compact($provider));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Provider $provider)
+    {
+        return view('admin.provider.edit',compact($provider));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRequest $request, Provider $provider)
+    {
+        $provider->update($request->all());
+        return redirect()->route('providers.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Provider $provider)
+    {
+        $provider->delete();
+        return redirect()->route('providers.index');
+    }
+}
---
+++ app/Http/Requests/Provider/UpdateRequest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Requests\Provider;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:250'],
+            'email' => ['required', 'email', 'max:250', 'unique:providers'],
+            'ruc_number' => ['required', 'string', 'max:11', 'min:11', 'unique:providers,ruc_number,' . $this->route('provider')->id],
+            'address' => ['nullable', 'string', 'max:250','unique:providers,ruc_number,' . $this->route('provider')->id],
+            'phone' => [
+                'required', 'string', 'max:9', 'min:9',
+                'unique:providers,phone,' . $this->route('provider')->id
+            ],
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'El campo nombre es obligatorio.',
+            'name.string' => 'El campo nombre debe ser una cadena de texto.',
+            'name.max' => 'El campo nombre no debe exceder los 250 caracteres.',
+            'email.required' => 'El campo correo electrónico es obligatorio.',
+            'email.email' => 'El campo correo electrónico debe ser una dirección de correo válida.',
+            'email.max' => 'El campo correo electrónico no debe exceder los 250 caracteres.',
+            'email.unique' => 'Ya existe un proveedor con este correo electrónico.',
+            'ruc_number.required' => 'El campo número de RUC es obligatorio.',
+            'ruc_number.string' => 'El campo número de RUC debe ser una cadena de texto.',
+            'ruc_number.max' => 'El campo número de RUC debe tener 11 caracteres.',
+            'ruc_number.min' => 'El campo número de RUC debe tener 11 caracteres.',
+            'ruc_number.unique' => 'Ya existe un proveedor con este número de RUC.',
+            'address.string' => 'El campo dirección debe ser una cadena de texto.',
+            'address.max' => 'El campo dirección no debe exceder los 250 caracteres.',
+            'address.unique' => 'Ya existe un proveedor con esta dirección.',
+            'phone.required' => 'El campo teléfono es obligatorio.',
+            'phone.string' => 'El campo teléfono debe ser una cadena de texto.',
+            'phone.max' => 'El campo teléfono debe tener 9 caracteres.',
+            'phone.min' => 'El campo teléfono debe tener 9 caracteres.',
+            'phone.unique' => 'Ya existe un proveedor con este número de teléfono.',
+        ];
+    }
+}
